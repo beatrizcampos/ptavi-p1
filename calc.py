05ae67e40d0099fc9efd4cf1b52e3e296c7d4bff
beatriz campos leon <bcampos@zeta40.aulas.gsyc.es>
2015-09-21 11:03:00 +0200

    Operacion suma

--- calc.py
+++ calc.py
@@ -3,8 +3,16 @@
 
 # Vamos a programar una calculadora
 
-def main():
-    pass
+import sys
 
-if __name__ == "__main__":
-    main()
+def main():
+    print("calculadora")
+
+def suma(num1, num2):
+    return num1 + num2
+
+
+if __name__ == "__main__": 
+    Operando1 = int(sys.argv[2])
+    Operando2 = int(sys.argv[3])
+    print(suma(Operando1,Operando2))
